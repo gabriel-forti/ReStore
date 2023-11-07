1f7a66ba12ca031113f9f1df4bd5e36fae550b01
Gabriel Benjamin Siqueira Forti <gabriel.forti@gatec.com.br>
2023-11-07 13:16:33 -0300

    ajustes section 5

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,15 +1,22 @@
 import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product"
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
 
 export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     // this happens everytime the component renders itself
     useEffect(() => { 
-        agent.Catalog.list().then(products => setProducts(products))
+        agent.Catalog.list()
+        .then(products => setProducts(products))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
     }, []);    
+
+    if(loading) return <LoadingComponent message="Loading products..." />
 
     return (  
         <>        
